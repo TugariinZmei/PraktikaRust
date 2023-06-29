f6ee407dcd71380b3c27b32e574315f0a0c76f12
TugarinZmei <40515625+TugariinZmei@users.noreply.github.com>
2023-06-29 21:46:25 +0300

    Add files via upload

---
+++ HW1/target/debug/HW1.d
@@ -0,0 +1,1 @@
+C:\Users\artem\PycharmProjects\HW1\target\debug\HW1.exe: C:\Users\artem\PycharmProjects\HW1\src\main.rs
---
+++ HW1/target/debug/deps/HW1.d
@@ -0,0 +1,5 @@
+C:\Users\artem\PycharmProjects\HW1\target\debug\deps\HW1.exe: src\main.rs
+
+C:\Users\artem\PycharmProjects\HW1\target\debug\deps\HW1.d: src\main.rs
+
+src\main.rs:
